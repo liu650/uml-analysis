2f4d29497b83296f33bcdd63f9164e6d45aa43c5
Marta <yao@alumni.ubc.ca>
2020-06-13 15:23:29 -0700

    add files back

---
+++ src/Field.java
@@ -0,0 +1,39 @@
+public class Field {
+    private String modifier;
+    private String type;
+    private String name;
+
+    public Field(String a, String b, String c){
+        this.modifier = a;
+        this.type = b;
+        this.name = c;
+
+    }
+
+    public Field() {
+    }
+
+    public String getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(String modifier) {
+        this.modifier = modifier;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyClass {
+
+    // TODO
+    private List<Field> fields;
+    // done
+    private List<Method> methods;
+    // TODO
+    private List<Object> importList;
+    // done
+    private String className;
+    // TODO
+    private String classType;
+    // TODO
+    private  List<Object> dependencyList;
+
+
+    MyClass(){
+        fields = new ArrayList<>();
+        methods = new ArrayList<>();
+        importList = new ArrayList<>();
+        dependencyList = new ArrayList<>();
+    }
+
+
+
+    public List<Field> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<Field> fields) {
+        this.fields = fields;
+    }
+
+    public List<Method> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(List<Method> methods) {
+        this.methods = methods;
+    }
+
+    public List<Object> getImportList() {
+        return importList;
+    }
+
+    public void setImportList(List<Object> importList) {
+        this.importList = importList;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+
+    public List<Object> getDependencyList() {
+        return dependencyList;
+    }
+
+    public void setDependencyList(List<Object> dependencyList) {
+        this.dependencyList = dependencyList;
+    }
+
+
+
+}
